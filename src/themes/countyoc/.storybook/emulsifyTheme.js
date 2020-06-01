4fa9260f5397497379dd9455cbecb83f513316f7
acouch <acinternets@gmail.com>
2020-06-01 00:24:17 -0400

    Removes old items

--- src/themes/countyoc/.storybook/emulsifyTheme.js
+++ src/themes/countyoc/.storybook/emulsifyTheme.js
@@ -5,7 +5,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   // Branding
-  brandTitle: 'Orange County',
-  brandUrl: 'https://oc-design.prometdev.com',
-  brandImage: 'https://user-images.githubusercontent.com/512243/74858075-ae7a7880-5312-11ea-8354-f6503fa65e91.png',
+  brandTitle: 'Promet Source',
+  brandUrl: 'https://prometsource.com',
+  brandImage: '/images/logo-promet.png',
 });
