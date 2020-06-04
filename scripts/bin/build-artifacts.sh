036f61d189ac025ea9309131f2411b625c17dd78
Aaron Couch <acinternets@gmail.com>
2020-06-04 12:17:49 -0400

    Update build-artifacts.sh

--- scripts/bin/build-artifacts.sh
+++ scripts/bin/build-artifacts.sh
@@ -2,7 +2,6 @@
 set -e
 
 composer install --no-dev
-cp ${PROJECT_ROOT}/src/htaccess ${PROJECT_ROOT}/.htaccess
 ${PROJECT_ROOT}/scripts/bin/build-theme.sh
 echo "Removing node modules"
 rm -rf ${PROJECT_ROOT}/${DOCROOT}/themes/custom/countyoc/node_modules
