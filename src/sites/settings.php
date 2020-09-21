1f50b0219b5df46bbc9f3f0a3c6e546661d33f51
Aaron Couch <acinternets@gmail.com>
2020-09-20 20:15:29 -0400

    Adds pantheon settings

--- src/sites/settings.php
+++ src/sites/settings.php
@@ -1,13 +1,26 @@
 <?php
 
 // @codingStandardsIgnoreFile
+//
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
 
-// DB and site specific settings.
-if (file_exists($app_root . '/' . $site_path . '/settings.site.php')) {
-  include $app_root . '/' . $site_path . '/settings.site.php';
-}
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all envrionments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to insure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
+
+
 // Dev settings.
-if (getenv('SITE_ENVIRONMENT') == 'local' && !getenv('SITE_ENVIRONMENT')) {
+if (getenv('SITE_ENVIRONMENT') == 'local' || !getenv('SITE_ENVIRONMENT')) {
   include $app_root . '/sites/settings.local.php';
 }
 // Drupal settings.
