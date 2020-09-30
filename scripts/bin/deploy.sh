ef41d25e3f0c80ff8ba05d1e7fb8dca12574aaf7
acouch <acinternets@gmail.com>
2020-09-30 09:21:49 -0400

    Updates deploy

--- scripts/bin/deploy.sh
+++ scripts/bin/deploy.sh
@@ -5,61 +5,18 @@ REMOTE="master"
 CURRENT_BRANCH=`git name-rev --name-only HEAD`
 CURRENT_TAG=`git name-rev --tags --name-only $(git rev-parse HEAD)`
 
-prep ()
+push (i)
 {
-  git remote add github https://${GH_TOKEN}@github.com/promet/provus.git 
-}
-
-quiet_git() {
-  stdout=$(tempfile)
-  stderr=$(tempfile)
-
-  if ! git "$@" </dev/null >$stdout 2>$stderr; then
-      cat $stderr >&2
-      rm -f $stdout $stderr
-      exit 1
-  fi
-
-  rm -f $stdout $stderr
-}
-
-add()
-{
-  quiet_git add --force -A docroot vendor scripts .docksal load.environment.php drush
-  quiet_git commit -m "Updates build"
-}
-
-build ()
-{
-  ${PROJECT_ROOT}/scripts/bin/build-artifacts.sh
-}
-
-branch ()
-{
-  prep
-  git checkout --orphan $BUILD_BRANCH
-  build
-  add
-  echo "Pushing branch to build..."
-  git push --force github $BUILD_BRANCH
-}
-
-tag ()
-{
-  prep
-  git tag -d $CURRENT_TAG
-  build
-  add
-  echo "Updating tag ..."
-  git tag $CURRENT_TAG
-  git push $CURRENT_TAG
+  git add .
+  git commit -m "Build for $i"
+  git push deploy $REMOTE 
 }
 
 if [ $CURRENT_TAG != "undefined" ]
 then
-  tag
+  push($CURRENT_TAG)
 elif [ $CURRENT_BRANCH == $DEV_BRANCH ]
 then
-  branch
+  push($CURRENT_BRANCH)
 fi
 
