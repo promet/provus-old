5b502db607cf820f6a044ec570e617463ec8ce50
Aaron Couch <acinternets@gmail.com>
2020-10-07 09:47:23 -0400

    Update build-artifacts.sh

--- scripts/bin/build-artifacts.sh
+++ scripts/bin/build-artifacts.sh
@@ -1,7 +1,6 @@
 # Abort if anything fails
 set -e
 
-THEME_NAME='provus'
 ${PROJECT_ROOT}/scripts/bin/build-theme.sh
 echo "Removing node modules"
 rm -rf ${PROJECT_ROOT}/src/themes/$THEME_NAME/node_modules
