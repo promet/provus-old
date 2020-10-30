07ae68cca7e919d53a879a082d00859084b3a1f9
acouch <acinternets@gmail.com>
2020-10-30 10:59:35 -0400

    Adds new-site command

---
+++ scripts/bin/new-site.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ${THEME_NAME}
+echo $THEME_NAME
+
+#1. Copy the theme
+cp -r src/themes/provus src/themes/${THEME_NAME}
+
+#2. Update theme filenames: 
+rename "s/provus/${THEME_NAME}/" src/themes/${THEME_NAME}/*.*
+
+#3. Update theme strings: 
+sed -i "s/provus/${THEME_NAME}/g" src/themes/${THEME_NAME}/*.*
+
+#4. Update blocks 
+sed -i "s/provus/${THEME_NAME}/g" config/default/block.*.yml
+
+#5. Update default theme 
+sed -i "s/provus/${THEME_NAME}/g" config/default/theme.settings.yml
+
+#5. Add "${THEME_NAME}" in theme section to 
+sed -i "s/provus/${THEME_NAME}/g" config/default/core.extension.yml
+
+#6. Update responsive images 
+sed -i "s/provus/${THEME_NAME}/g" config/default/responsive_image.styles.*.yml
+
+#7. Update composer.json
+sed -i "s/provus/${THEME_NAME}/g" composer.json 
+
