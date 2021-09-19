e9b1bd26a6430085e3d9c52899be5784203d5636
Travis CI User <travis@example.org>
2021-09-19 19:21:22 +0000

    Updates storybook

--- docs/storybook/react-syntax-highlighter_languages_highlight_vbscriptHtml.247d37098f251b6fbfaf.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{1789:function(n,e){n.exports=function(n){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
