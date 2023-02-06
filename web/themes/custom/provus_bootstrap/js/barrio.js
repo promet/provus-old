4378fa6654904c6b3ada822427feb40d1bb2905c
John Lutz <jlutz@prometsource.com>
2023-02-06 09:23:14 -0600

    CTA Theming: Fix template not showing.  Fix subtitle display.

--- web/themes/custom/provus_bootstrap/js/barrio.js
+++ web/themes/custom/provus_bootstrap/js/barrio.js
@@ -3,38 +3,67 @@
  * Global utilities.
  *
  */
-(function ($, Drupal) {
+(function (Drupal) {
 
   'use strict';
 
   Drupal.behaviors.bootstrap_barrio = {
     attach: function (context, settings) {
 
-      var position = $(window).scrollTop();
-        $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-          $('body').addClass("scrolled");
+      var position = window.scrollY;
+      window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.querySelector('body').classList.add("scrolled");
         }
         else {
-          $('body').removeClass("scrolled");
+          document.querySelector('body').classList.remove("scrolled");
         }
-        var scroll = $(window).scrollTop();
+        var scroll = window.scrollY;
         if (scroll > position) {
-          $('body').addClass("scrolldown");
-          $('body').removeClass("scrollup");
+          document.querySelector('body').classList.add("scrolldown");
+          document.querySelector('body').classList.remove("scrollup");
         } else {
-          $('body').addClass("scrollup");
-          $('body').removeClass("scrolldown");
+          document.querySelector('body').classList.add("scrollup");
+          document.querySelector('body').classList.remove("scrolldown");
         }
         position = scroll;
       });
 
-      $('.dropdown-item a.dropdown-toggle').on("click", function (e) {
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-      });
+      document.addEventListener('click', function (event) {
+
+        // If the clicked element doesn't have the right selector, bail
+        if (!event.target.matches('.dropdown-item a.dropdown-toggle')) return;
+      
+        // Don't follow the link
+        event.preventDefault();
+      
+        toggle(event.target.next('ul'));
+        event.stopPropagation();
+      
+      }, false);
+      
+      // Toggle element visibility
+      var toggle = function (elem) {
+
+        // If the element is visible, hide it
+        if (window.getComputedStyle(elem).display === 'block') {
+          hide(elem);
+          return;
+        }
+
+        // Otherwise, show it
+        show(elem);
+
+      };
+      
+      var show = function (elem) {
+        elem.style.display = 'block';
+      };
+      
+      var hide = function (elem) {
+        elem.style.display = 'none';
+      };
     }
   };
 
-})(jQuery, Drupal);
+})(Drupal);
