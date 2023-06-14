cedd23238e9e3de799af5bf9ba9f93974b084018
Aaron Armstrong <296-aastrong@users.noreply.drupalcode.org>
2023-06-14 15:45:49 +0000

    Remove DB details

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -14,14 +14,3 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 
 $settings['config_sync_directory'] = '../config/default';
 
-// Docksal DB connection settings.
-$databases['default']['default'] = array (
-  'database' => 'default',
-  'username' => 'user',
-  'password' => 'user',
-  'prefix' => '',
-  'host' => 'db',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
